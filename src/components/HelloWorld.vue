69ec060556e911bb17c6287b65afad423b55cd2f
Ilya Durdyev <funbanji@gmail.com>
2018-03-18 23:07:02 +0300

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-button type="primary" @click="visible = true">Button</el-button>
+    <el-slider v-model="value1"></el-slider>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      value1: 0,
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
