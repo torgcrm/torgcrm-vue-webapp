9ff9885ea6d1daa07c3b6db768e545eb1d415752
Ilya Durdyev <funbanji@gmail.com>
2018-03-19 09:09:06 +0300

    Menu component and test element-ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>
-    <el-button type="primary" @click="visible = true">Button</el-button>
-    <el-slider v-model="value1"></el-slider>
+  <div class="container">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="250px">
+          <side-menu />
+        </el-aside>
+        <el-container>
+          <el-main>
+            <orders-table />
+          </el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import SideMenu from './SideMenu'
+import OrdersTable from "./OrdersTable";
+
 export default {
+  components: {
+    OrdersTable,
+    SideMenu},
   name: 'HelloWorld',
   data () {
     return {
@@ -16,3 +34,17 @@ export default {
   }
 }
 </script>
+
+<style>
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+</style>
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-menu
+    default-active="2"
+    class="side-menu"
+    @open="handleOpen"
+    @close="handleClose">
+    <el-menu-item-group title="CRM">
+      <el-menu-item>
+        <i class="el-icon-location"></i>
+        <span>Заказы</span>
+      </el-menu-item>
+      <el-menu-item>
+        <i class="el-icon-location"></i>
+        <span>Клиенты</span>
+      </el-menu-item>
+    </el-menu-item-group>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'side-menu'
+}
+</script>
+
+<style scoped>
+.side-menu {
+  height: 100%;
+}
+</style>
